a777549e02c370e9d90d744174db5e45a0be0073
Jae <giffordjacob0@gmail.com>
2022-11-21 15:48:53 +0800

    shoe and category mongoose schemas added

---
+++ models/shoe.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ShoeSchema = new Schema({
+  name: { type: String, required: true, maxLength: 50 },
+  price: { type: Number },
+  description: { type: String },
+  size: { type: Float64Array },
+});
+
+// Virtual for shoes' URL
+AuthorSchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/shoe/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model('Shoe', ShoeSchema);
